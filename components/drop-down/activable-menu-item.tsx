bdc2eee5af32042511f48e3b35af0ba049b70b79
Masood Rezazadeh <rezazadehmasood@gmail.com>
2021-05-08 14:08:58 +0430

    Simplify dropdown color settings

--- components/drop-down/activable-menu-item.tsx
+++ components/drop-down/activable-menu-item.tsx
@@ -17,26 +17,14 @@ const ActivableMenuItem: FC<ActivableMenuItemProps> = ({
 }) => (
   <Menu.Item disabled={disabled} onClick={() => onSelect && onSelect(item)}>
     {({ active }) => {
-      const activeAndSelectedClasses =
-        active && selected ? "bg-indigo-500 text-white" : "";
-
-      const selectedClasses = selected
-        ? active
-          ? activeAndSelectedClasses
-          : "bg-indigo-400 text-white"
-        : "";
-
-      const activeClasses = active ? "bg-indigo-500 text-white" : "";
-
-      const notActiveOrSelectedClasses =
-        !active && !selected ? "bg-white text-gray-900" : "";
+      const classes = active
+        ? "bg-indigo-400 text-white"
+        : selected
+        ? "bg-indigo-500 text-white"
+        : "bg-white text-gray-900";
 
       return (
-        <a
-          className={`flex items-center p-1 px-4 ${activeClasses} ${selectedClasses} ${activeAndSelectedClasses} ${notActiveOrSelectedClasses}`}
-        >
-          {item.title}
-        </a>
+        <a className={`flex items-center p-1 px-4 ${classes}`}>{item.title}</a>
       );
     }}
   </Menu.Item>
